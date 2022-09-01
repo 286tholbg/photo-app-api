178d33798ad2ea05c2ce8fcd24e2d5e2cc786fbe
286tholbg <107855431+286tholbg@users.noreply.github.com>
2022-09-01 17:00:52 +1000

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from '../logo.svg'
+
+export default function Navbar(props) {
+    return (
+         <nav className={props.darkMode ? "dark": ""}>
+            <img 
+                className="nav--logo_icon"
+                src={logo}
+            />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            
+            <div className="toggler">
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}>
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+        </nav>
+    )
+}
